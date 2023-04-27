6cba8af1efe13d73efe749e0a81c1883d56ae695
Raterfy <roberthak02@gmail.com>
2023-04-27 19:16:03 +0200

    putstr plus court

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:00:18 by robhak            #+#    #+#             */
-/*   Updated: 2023/04/26 21:00:43 by robhak           ###   ########.fr       */
+/*   Updated: 2023/04/27 18:51:11 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,11 @@
 
 /*Cette fonction écrit la chaîne de caractères 
 's' sur le descripteur de fichier donné.*/
-
 void	ft_putstr_fd(char *s, int fd)
 {
 	size_t	len;
 
 	if (!s)
 		return ;
-	len = 0;
-	while (s[len])
-		len++;
-	write(fd, s, len);
+	write(fd, s, ft_strlen(s));
 }
