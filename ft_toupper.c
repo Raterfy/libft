c040d48a9e854109a13c5a95696591ff55538c42
Raterfy <roberthak02@gmail.com>
2023-05-07 02:52:22 +0200

    template pour expliquer les toupper et lower fonction

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,27 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:27:41 by robhak            #+#    #+#             */
-/*   Updated: 2023/04/25 13:27:42 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/07 02:50:50 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Convertit le caractère c en majuscule s'il est une lettre minuscule */
+/*
+** Nom de la fonction: ft_toupper
+** Paramètre(s): int c
+** Valeur de retour: int
+** Description: Cette fonction convertit une lettre minuscule 
+	en majuscule si possible.
+** Elle prend en paramètre un caractère c et renvoie sa version 
+	majuscule si c est une lettre minuscule.
+** Sinon, elle renvoie c inchangé.
+** Elle utilise la table ASCII pour comparer et convertir les caractères.
+** Exemple(s):
+** ft_toupper('a'); // renvoie 'A'
+** ft_toupper('Z'); // renvoie 'Z'
+** ft_toupper('?'); // renvoie '?'
+*/
 int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
